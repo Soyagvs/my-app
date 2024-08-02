d184e68f5d5dc58b650c21721ced4aaefe62322b
Soyagvs <agustinmartinezyt@gmail.com>
2024-08-02 03:36:42 -0300

    create navigation aside

---
+++ src/ui/Aside.jsx
@@ -0,0 +1,57 @@
+'use client'
+
+import Link from "next/link"
+import Image from "next/image"
+import { usePathname } from "next/navigation"
+import { useRouter } from "next/navigation"
+
+export const Aside = () => {
+    const pathname = usePathname()
+    const href = useRouter()
+    return (
+        <section className="w-[320px] h-full bg-gray-100">
+            <div className="flex flex-col justify-center items-center mt-5">
+                <Image src="/img/user.svg" width={100} height={100} alt="Imagen de perfil" />
+                <h2>Agustin</h2>
+            </div>
+            {``}
+            <nav className="mt-16">
+                <ol className="flex flex-col justify-center items-start text-2xl font-semibold">
+                    <Link href="/users/dashboard/tables-availables" className={`pl-3 flex justify-start items-center gap-3 w-full h-16 hover:bg-blue-100 ${pathname === "/users/dashboard/tables-availables" ? 'bg-blue-400 hover:bg-blue-400' : ''}`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
+                        Mesas disponibles
+                        </Link>
+                    <Link href="/users/dashboard/tables-in-use" className={`pl-3 flex justify-start items-center gap-3 w-full h-16 hover:bg-blue-100 ${pathname === "/users/dashboard/tables-in-use" ? 'bg-blue-400 hover:bg-blue-400' : ''}`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                    </svg>
+                        Mesas en uso</Link>
+                    <Link href="/users/dashboard/closed-tables" className={`pl-3 flex justify-start items-center gap-3 w-full h-16 hover:bg-blue-100 ${pathname === "/users/dashboard/closed-tables" ? 'bg-blue-400 hover:bg-blue-400' : ''}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                        </svg>
+                        Mesas cerradas</Link>
+                    <Link href="/users/dashboard/statistics" className={`pl-3 flex justify-start items-center gap-3 w-full h-16 hover:bg-blue-100 ${pathname === "/users/dashboard/statistics" ? 'bg-blue-400 hover:bg-blue-400' : ''}`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+                    </svg>
+                        Estadisticas</Link>
+                    <Link href="/users/dashboard/bills" className={`pl-3 flex justify-start items-center gap-3 w-full h-16 hover:bg-blue-100 ${pathname === "/users/dashboard/bills" ? 'bg-blue-400 hover:bg-blue-400' : ''}`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                        Gastos</Link>
+                    <div className="flex flex-col justify-center items-start mt-80 w-full">
+                        <Link href="/users/dashboard/settings" className={`pl-3 flex justify-start items-center gap-3 w-full h-16 hover:bg-blue-100 ${pathname === "/users/dashboard/settings" ? 'bg-blue-400 hover:bg-blue-400' : ''}`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+                        </svg>
+                            Configuracion</Link>
+                        <Link href="/users/" className={`pl-3 flex justify-start items-center gap-3 w-full h-16 hover:bg-blue-100 ${pathname === "/users/" ? 'bg-blue-400' : ''}`}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                            Cerrar sesion</Link>
+                    </div>
+                    
+                </ol>
+            </nav>
+        </section>
+    )
+}
