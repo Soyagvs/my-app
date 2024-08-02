7b1dcb1f638607b3a4dbcf6a27fb33bcab9d09e1
Soyagvs <agustinmartinezyt@gmail.com>
2024-08-02 03:36:52 -0300

    create card components

---
+++ src/ui/CardInUse.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+export const CardInUse = () => {
+  return (
+      <div className="w-[300px] h-[380px] bg-[#CDCDCD] rounded-xl shadow-md shadow-slate-500 flex flex-col justify-center items-center hover:scale-105 duration-100">
+          <div className="flex justify-center items-center mb-10">
+              <p className="text-3xl font-semibold">MESA N &bull; 1</p>
+          </div>
+          <div className="flex justify-center items-center mb-10">
+              <Image src="/img/icon.png" width={100} height={100} alt="Imagen de mesa" />
+          </div>
+          <div className="flex flex-col justify-center items-center mb-[-10px] gap-2">
+              <div className="border w-64 border-b-black mb-3"></div>
+              <button className="w-52 h-10 rounded-md text-xl font-medium bg-blue-500">Ver mesa</button>
+              <button className="w-52 h-10 rounded-md text-xl font-medium bg-green-500">Cerrar mesa</button>
+          </div>
+      </div>
+  )
+}
+
