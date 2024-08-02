783c13f590ade847cd72d8331c196eae83f48ef4
Soyagvs <agustinmartinezyt@gmail.com>
2024-08-02 03:37:09 -0300

    create component navbar

---
+++ src/ui/Navigation.jsx
@@ -0,0 +1,13 @@
+
+export const Navigation = () => {
+    return (
+        <header>
+            <nav>
+                <ul className="flex gap-5 items-center p-5 shadow-md h-20">
+                    <li className="text-5xl font-bold ">Resto Mesas</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
